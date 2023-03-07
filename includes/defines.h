821a75d1e41afe207980e6189babee913a4ff1e5
tomatte <daniel.brandao2000@gmail.com>
2023-03-07 17:11:35 -0300

    add some defines

---
+++ includes/defines.h
@@ -0,0 +1,27 @@
+#ifndef DEFINES_H
+# defube DEFINES_H
+
+//TO BE USED AS TYPES AND STATES
+# define NONE 0
+# define WORD 1
+# define OPERATOR 2
+# define EXPANSIVE 3
+# define QUOTE 4
+
+//TO BE USED AS IDs
+# define EXEC 5
+# define ARG 6
+# define SIMPLE_QUOTES 7
+# define DOUBLE_QUOTES 8
+# define REDIR_IN 9
+# define REDIR_OUT 10
+# define HERE_DOC 11
+# define REDIR_AOUT 12
+# define PIPE 13
+# define ENV_VAR 14
+# define EXIT_STAT 15
+# define BUILTIN 16
+# define PATH_PROG 17
+# define FILE_PROG 18
+
+#endif
