8a130818ed0dad8da99ea40b28189a655cc5805a
tomatte <daniel.brandao2000@gmail.com>
2023-04-14 13:20:30 -0300

    code cleaning

--- src/parser/categorize_word.c
+++ src/parser/categorize_word.c
@@ -6,7 +6,7 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 16:53:14 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/03/13 16:30:34 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/04/14 13:18:40 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ static int	is_exec(t_token *token)
 	s = get_state();
 	if (s == START || s == PIPE)
 	{
-		set_state(EXEC);
 		token->id = EXEC;
 		return (1);
 	}
@@ -33,7 +32,6 @@ static int	is_infile(t_token *token)
 	s = get_state();
 	if (s == R_INPUT)
 	{
-		set_state(IN_FILE);
 		token->id = IN_FILE;
 		return (1);
 	}
@@ -47,7 +45,6 @@ static int	is_outfile(t_token *token)
 	s = get_state();
 	if (s == R_OUTPUT || s == R_APPEND_OUT)
 	{
-		set_state(OUT_FILE);
 		token->id = OUT_FILE;
 		return (1);
 	}
@@ -58,7 +55,6 @@ static int	is_heredoc_end(t_token *token)
 {
 	if (get_state() == HERE_DOC)
 	{
-		set_state(HERE_DOC_END);
 		token->id = HERE_DOC_END;
 		return (1);
 	}
@@ -70,13 +66,13 @@ void	categorize_word(t_token *token)
 	if (token->type != WORD && token->type != QUOTE)
 		return ;
 	if (is_exec(token))
-		;
+		set_state(EXEC);
 	else if (is_infile(token))
-		;
+		set_state(IN_FILE);
 	else if (is_outfile(token))
-		;
+		set_state(OUT_FILE);
 	else if (is_heredoc_end(token))
-		;
+		set_state(HERE_DOC_END);
 	else
 		token->id = ARG;
 }
