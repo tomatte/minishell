800e8ad2a60cbb1aa3d5b772e3e5ac9c145b75f7
tomatte <daniel.brandao2000@gmail.com>
2023-04-14 12:46:29 -0300

    improve unset error management

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 14:54:39 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/04/12 15:00:47 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/04/14 12:45:08 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,13 @@ void	unset(t_command *cmd)
 {
 	int	i;
 
+	if (is_option(cmd->args[1]))
+	{
+		invalid_option(cmd->args[0], cmd->args[1]);
+		return ;
+	}
 	i = 0;
 	while (cmd->args[++i])
 		remove_evar(cmd->args[i]);
+	set_error(0);
 }
