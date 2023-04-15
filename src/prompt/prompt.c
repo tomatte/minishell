597d3b42f2ea884068cdb7a18e018226dcae9f46
tomatte <daniel.brandao2000@gmail.com>
2023-04-15 16:50:00 -0300

    change prompt to handle ctrl + d behavior

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 10:37:08 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/03/27 16:16:13 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/04/15 16:47:49 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ char	*prompt(void)
 
 	msg = get_prompt_msg();
 	str_command = readline(msg);
+	if (str_command == NULL)
+	{
+		ft_printf("exit\n");
+		mini_exit(get_error());
+	}
 	free(msg);
 	add_history(str_command);
 	add_to_tracker(str_command);
