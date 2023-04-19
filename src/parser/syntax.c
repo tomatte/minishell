5340fd2cbb502b91188319c5df2fa89e3b0ad096
tomatte <daniel.brandao2000@gmail.com>
2023-04-19 17:04:26 -0300

    remove unused function

--- src/parser/syntax.c
+++ src/parser/syntax.c
@@ -6,21 +6,11 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 14:26:17 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/04/19 14:20:59 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/04/19 17:03:25 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-static int	is_operator_state(void)
-{
-	int	s;
-
-	s = get_state();
-	if (s >= HERE_DOC && s <= PIPE)
-		return (1);
-	return (0);
-}
 
 static int	is_redirec(int r)
 {
