820c24675375a47ea339603c05a3f30d01491f75
tomatte <daniel.brandao2000@gmail.com>
2023-04-23 11:16:29 -0300

    fix export bug

--- src/utils/export_utils.c
+++ src/utils/export_utils.c
@@ -6,7 +6,7 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 10:16:37 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/04/23 10:49:10 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/04/23 11:07:59 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ static char	*get_var_name(char *full_var)
 	int		i;
 
 	i = 0;
-	while (full_var[i] != '=')
+	while (full_var[i] && full_var[i] != '=')
 		i++;
+	if (full_var[i] == '\0')
+		return (NULL);
 	var_name = ft_calloc(i + 1, sizeof(char));
 	ft_strlcpy(var_name, full_var, i + 1);
 	return (var_name);
@@ -31,24 +33,26 @@ static void	replace_var_value(t_list *var, char *full_var)
 	var->content = (void *) ft_strdup(full_var);
 }
 
-static t_list	*find_old_var(char *full_var)
+static t_list	*find_old_var(char *var_name)
 {
 	t_list	*var;
-	char	*var_name;
 
-	var_name = get_var_name(full_var);
 	var = find_evar2(var_name);
-	free(var_name);
 	return (var);
 }
 
 void	add_var_to_evars(char *full_var)
 {
 	t_list	*var;
+	char	*var_name;
 
-	var = find_old_var(full_var);
+	var_name = get_var_name(full_var);
+	if (var_name == NULL)
+		return ;
+	var = find_old_var(var_name);
 	if (var)
 		replace_var_value(var, full_var);
 	else
 		add_evar2(full_var);
+	free(var_name);
 }
