caadd73a2639c155312bf7de76b30c178519e73f
tomatte <daniel.brandao2000@gmail.com>
2023-04-23 09:56:31 -0300

    exit code 2 when syntax error

--- src/error/more_errors.c
+++ src/error/more_errors.c
@@ -6,7 +6,7 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 14:47:29 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/04/22 16:11:33 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/04/23 09:49:17 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	nut_error(char *str)
 	ft_putstr_fd("bash: syntax error near unexpected token `", 2);
 	ft_putstr_fd(str, 2);
 	ft_putstr_fd("'\n", 2);
+	set_error(2);
 	set_state(ERR_NUT);
 }
 
