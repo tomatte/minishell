44b0931d79d904e44f81b166349fa5bf8a3f52f3
tomatte <daniel.brandao2000@gmail.com>
2023-04-30 01:39:36 +0200

    remove sigpipe ign

--- src/signals/set_signals.c
+++ src/signals/set_signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   set_signals.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: dbrandao < dbrandao@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 14:37:33 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/04/29 18:31:07 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/04/30 01:35:26 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,8 @@ static void	sig_handler(int sig)
 	rl_redisplay();
 }
 
-static void	sigpipe_handler(int sig)
-{
-	(void) sig;
-	mini_exit(0);
-}
-
 void	set_signals(void)
 {
 	signal(SIGQUIT, SIG_IGN);
 	signal(SIGINT, sig_handler);
-	signal(SIGPIPE, sigpipe_handler);
 }
